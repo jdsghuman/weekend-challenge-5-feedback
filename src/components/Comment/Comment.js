0d2a81f2e9985e91c74355a4265ff9aa60e69ee0
JD Ghuman <jdsghuman@gmail.com>
2018-12-09 06:28:28 -0600

    Add comment component and reducer

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Steps from '../Steps/Steps';
-import Rating from 'react-rating';
-import Star from '@material-ui/icons/Star';
-import StarBorder from '@material-ui/icons/StarBorder';
+import { connect } from 'react-redux';
 import './Comment.css';
 
 class Comment extends Component {
@@ -10,13 +8,13 @@ class Comment extends Component {
     comment: 0
   }
 
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  }
+
   handleNext = (event) => {
     this.props.dispatch({type: 'ADD_COMMENT', payload: this.state.comment});
     this.props.history.push('/review');
-  }
-
-  handleStarClick = (value) => {
-    this.setState({ comment: value });
   }
 
   render() {
@@ -24,11 +22,11 @@ class Comment extends Component {
       <div>
         <Steps step={"4"} />
         <h2>Any comments you want to leave?</h2>
-        <textarea onChange={this.handleStarClick} placeholder="Enter your comment here..." />
+        <textarea onChange={this.handleChange} placeholder="Enter your comment here..." />
         <button onClick={this.handleNext}>Next</button>
       </div>
     );
   }
 }
 
-export default Comment;
+export default connect()(Comment);
