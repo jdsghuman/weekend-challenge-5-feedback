a439eb4c1733cf94e4962fdf5f5b12d87e17f270
JD Ghuman <jdsghuman@gmail.com>
2018-12-10 18:59:25 -0600

    Add Heroku build

---
+++ build/precache-manifest.07f2ad5714c2b6ce4443aa603f7eb868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d831257860868a2c5d7",
+    "url": "/static/js/main.0d831257.chunk.js"
+  },
+  {
+    "revision": "1ad4167b4153520e75cc",
+    "url": "/static/js/1.1ad4167b.chunk.js"
+  },
+  {
+    "revision": "0d831257860868a2c5d7",
+    "url": "/static/css/main.14931f04.chunk.css"
+  },
+  {
+    "revision": "1b0237ef41474e96996342db878917bf",
+    "url": "/index.html"
+  }
+];
